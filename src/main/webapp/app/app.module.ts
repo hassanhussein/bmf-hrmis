9fd00c5d0e78b1954462076ee4f3d0eb7d69277c
chris <chrismelky05@gmail.com>
2020-08-29 15:03:41 +0300

    add material

--- src/main/webapp/app/app.module.ts
+++ src/main/webapp/app/app.module.ts
@@ -7,6 +7,9 @@ import { HrmisCoreModule } from 'app/core/core.module';
 import { HrmisAppRoutingModule } from './app-routing.module';
 import { HrmisHomeModule } from './home/home.module';
 import { HrmisEntityModule } from './entities/entity.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import 'hammerjs';
 // jhipster-needle-angular-add-module-import JHipster will add new module here
 import { MainComponent } from './layouts/main/main.component';
 import { NavbarComponent } from './layouts/navbar/navbar.component';
@@ -18,6 +21,8 @@ import { ErrorComponent } from './layouts/error/error.component';
 @NgModule({
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
     HrmisSharedModule,
     HrmisCoreModule,
     HrmisHomeModule,
--- src/main/webapp/app/shared/shared.module.ts
+++ src/main/webapp/app/shared/shared.module.ts
@@ -5,9 +5,17 @@ import { AlertComponent } from './alert/alert.component';
 import { AlertErrorComponent } from './alert/alert-error.component';
 import { HasAnyAuthorityDirective } from './auth/has-any-authority.directive';
 
+import { JhMaterialModule } from 'app/shared/jh-material.module';
 @NgModule({
-  imports: [HrmisSharedLibsModule],
+  imports: [JhMaterialModule, HrmisSharedLibsModule],
   declarations: [FindLanguageFromKeyPipe, AlertComponent, AlertErrorComponent, HasAnyAuthorityDirective],
-  exports: [HrmisSharedLibsModule, FindLanguageFromKeyPipe, AlertComponent, AlertErrorComponent, HasAnyAuthorityDirective],
+  exports: [
+    JhMaterialModule,
+    HrmisSharedLibsModule,
+    FindLanguageFromKeyPipe,
+    AlertComponent,
+    AlertErrorComponent,
+    HasAnyAuthorityDirective,
+  ],
 })
 export class HrmisSharedModule {}
